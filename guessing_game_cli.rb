a1a8f12253eb76c5a9e43cdf1846acc7c24327e8
Maya Blevins <maya.blevins@gmail.com>
2020-05-07 18:42:31 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,6 +7,6 @@ def run_guessing_game
   
   if input === random_num
     return "You guessed the correct number!"
-  else
-    return "Sorry! The computer guessed"
+  elsif
+    return "Sorry! The computer guessed #{random_num}."
 end
