93f2a7b53097440fa15eff3da5758ab66bc61c70
Maya Blevins <maya.blevins@gmail.com>
2020-05-07 18:41:29 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,10 @@
 # Code your solution here!
+def run_guessing_game
+  random_num = rand(1..6)
+  
+  puts "Please pick a number 1-6"
+  input = gets.chomp 
+  
+  if input === random_num
+    return 
+end
