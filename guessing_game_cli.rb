b104f5521878a3a3b059eb9c98f07fd46f351384
Maya Blevins <maya.blevins@gmail.com>
2020-05-07 19:16:54 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,18 +1,15 @@
 # Code your solution here!
 def run_guessing_game
   random_num = rand(1..6)
-  # prompt_user()
-  puts "Please pick a number 1-6"
-  # input = get_input()
+  prompt_user()
+  input = get_input()
   input = gets.chomp.to_i
   if input === random_num
     puts "You guessed the correct number!"
-  #  elsif input === "exit" || "EXIT"
-    # return "Goodbye!"
-    # run_guessing_game
+    run_guessing_game
   elsif (1..6).include?(input) && input != random_num
     puts "Sorry! The computer guessed #{random_num}."
-    # run_guessing_game
+    run_guessing_game
   else
     input === "exit"
     return "Goodbye!"
