7650426aad3dac1f8c361a743a8776462a99b977
Maya Blevins <maya.blevins@gmail.com>
2020-05-07 18:47:40 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,8 +3,7 @@ def run_guessing_game
   random_num = rand(1..6)
   
   puts "Please pick a number 1-6"
-  input = gets.chomp 
-  p input
+  input = gets.chomp
   if input.to_i === random_num
     return "You guessed the correct number!"
   elsif input === "exit" || "EXIT"
